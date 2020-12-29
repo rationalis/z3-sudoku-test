0af5cfed7348d974caf35bd57aa5281e4ed0b92d
Jimmy Ye <jiy162@ucsd.edu>
2020-12-28 16:33:56 -0800

    Some refactoring/cleanup

--- sudoku_z3.py
+++ sudoku_z3.py
@@ -1,32 +1,58 @@
 #!/usr/bin/env python3
 
-from sudoku import Sudoku
+from sudoku import Sudoku, rows, cols
 
 from z3 import Solver, Int, Or, Distinct, sat
 
-# first we build a solver with the general constraints for sudoku puzzles:
-solver = Solver()
-
 symbols = {pos: Int(pos) for pos in Sudoku.positions}
 
-# assure that every cell holds a value of [1,9]
-for symbol in symbols.values():
-    solver.add(Or([symbol == i for i in range(1, 10)]))
+def basic_solver(solver=None):
+    if not solver:
+        # first we build a solver with the general constraints for sudoku puzzles:
+        solver = Solver()
 
-# assure that every row covers every value:
-for row in "ABCDEFGHI":
-    solver.add(Distinct([symbols[row + col] for col in "123456789"]))
+    # assure that every cell holds a value of [1,9]
+    for symbol in symbols.values():
+        solver.add(Or([symbol == i for i in range(1, 10)]))
 
-# assure that every column covers every value:
-for col in "123456789":
-    solver.add(Distinct([symbols[row + col] for row in "ABCDEFGHI"]))
+    # assure that every row covers every value:
+    for row in rows:
+        solver.add(Distinct([symbols[row + col] for col in cols]))
 
-# assure that every block covers every value:
-for i in range(3):
-    for j in range(3):
-        solver.add(Distinct([symbols["ABCDEFGHI"[m + i * 3] + "123456789"[n + j * 3]] for m in range(3) for n in range(3)]))
+    # assure that every column covers every value:
+    for col in cols:
+        solver.add(Distinct([symbols[row + col] for row in rows]))
 
-def z3_solving(puzzle):
+    # assure that every block covers every value:
+    for i in range(3):
+        for j in range(3):
+            solver.add(Distinct([symbols[rows[m + i * 3] + cols[n + j * 3]] for m in range(3) for n in range(3)]))
+
+    return solver
+
+def with_hidden_singles(solver=None):
+    if not solver:
+        solver = basic_solver()
+
+    # every row contains each value in some column:
+    for row in rows:
+        for v in range(1, 10):
+            solver.add(Or([symbols[row + col] == v for col in cols]))
+
+    # every column contains each value in some row:
+    for col in cols:
+        for v in range(1, 10):
+            solver.add(Or([symbols[row + col] == v for row in rows]))
+
+    # assure that every block covers every value:
+    for i in range(3):
+        for j in range(3):
+            for v in range(1, 10):
+                solver.add(Or([symbols[rows[m + i * 3] + cols[n + j * 3]] == v for m in range(3) for n in range(3)]))
+
+    return solver
+
+def z3_solving(puzzle, solver):
     """ Function solving the given sudoku puzzle using Z3 """
 
     # Save current state so we can erase the given values and re-use the basic
@@ -46,6 +72,26 @@ def z3_solving(puzzle):
 
     return Sudoku(values)
 
+def timed_solve(puzzles, solver, solver_name='Z3'):
+    count = len(puzzles)
+    print(f'[+] solving using {solver_name}')
+
+    solutions = []
+
+    from time import perf_counter as now
+    millis_from_secs = 1000
+    start = now()
+    for (i, puzzle) in enumerate(puzzles):
+        solutions.append(z3_solving(puzzle, solver))
+        if count > 1000 and i % 1000 == 0 and i > 0:
+            print(f'[+] solved puzzle {i}')
+    end = now()
+    for solution in solutions:
+        assert solution.is_solved()
+    avg = round(((end - start)/count)*millis_from_secs, 6)
+    print(f'{avg}ms average solve time for {count} puzzles')
+    return solutions
+
 def main(argv):
     puzzles = []
     with open('1106_375.txt', 'r') as f:
@@ -61,30 +107,10 @@ def main(argv):
                 print(f'[+] parsing puzzle {i}')
             puzzles.append(Sudoku(string=puzzle))
 
-    print("[+] solving using Z3")
+    timed_solve(puzzles, basic_solver(), 'basic Z3')
+    solutions = timed_solve(puzzles, with_hidden_singles(), 'hidden singles Z3')
+    timed_solve(solutions, basic_solver(), 'complete solution checker')
 
-    solutions = []
-
-    from time import perf_counter as now
-    millis_from_secs = 1000
-    start = now()
-    for (i, puzzle) in enumerate(puzzles):
-        solutions.append(z3_solving(puzzle))
-        if count > 1000 and i % 1000 == 0 and i > 0:
-            print(f'[+] solved puzzle {i}')
-    end = now()
-    for solution in solutions:
-        assert solution.is_solved()
-    avg = round(((end - start)/count)*millis_from_secs, 6)
-    print(f'{avg}ms average solve time for {count} puzzles')
-
-    millis_from_secs = 1000
-    start = now()
-    for puzzle in solutions:
-        z3_solving(puzzle)
-    end = now()
-    avg = round(((end - start)/count)*millis_from_secs, 6)
-    print(f'{avg}ms average check time for {count} puzzles')
 
 if __name__ == "__main__":
     from sys import argv
